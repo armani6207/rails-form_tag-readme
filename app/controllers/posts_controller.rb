978c16db46d64f79de79563ba7f111e687b7d825
Armando <rojas.armani@gmail.com>
2021-04-15 16:57:34 -0400

    commit tree 5faa48e311f9c2aa2f1a7e09613d59b744142e23

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,15 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+
+  end
+
+  def create
+    params
+    Post.create(title: params["post"]["title"], description: params["post"]["description"])
+    redirect_to :posts
+  end
   
 end
